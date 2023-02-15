6e83816ab9c28a93d8f1441e67037c854bd90a14
jared2468 <blueesp@gmail.com>
2023-02-14 17:08:59 -0800

    added more answers to questions

--- week3codingassignment.js
+++ week3codingassignment.js
@@ -10,28 +10,35 @@ console.log('ages array: ' + ages); //print array
 // 1a.Programmatically subtract the value of the first element in the array from the value in the last element of the array.   
 // Do not use numbers to reference the last element, find it programmatically.
 // ages[7] – ages[0] is not allowed
-function sub(ages){
-    return ages.lenght-1 - ages.slice(0,1)
-}
-//console.log(ages[ages.length-1] - ages.slice(0,1));
+
+console.log(ages[ages.length-1] - ages.slice(0,1));// subtract 1st element using slice from last using length-1, should be 90
 
 //let x = (ages[ages.length-1] - ages[0]); //subract the 1st element from the last
 //console.log('Q1a. Subract the 1st element from the last: ' + x); //print the result
 // 1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
-ages.push(14);
-console.log(sub);
+//ages.push(14);
+ages.push(14); // add new age to array, 14
+console.log(ages); //print array with new age
+console.log(ages[ages.length-1] - ages.slice(0,1));//I think should go in a function but not sure how to make that work?
+//console.log(sub);
 //console.log(ages[ages.length-1] - ages.slice(0,1))
 // 1c. Use a loop to iterate through the array and calculate the average age. 
 // 2.  Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
 // 2a. Use a loop to iterate through the array and calculate the average number of letters per name. 
 // 2b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces. 
-// 3.  How do you access the last element of any array?
+// 3.  How do you access the last element of any array? 
+console.log(ages[ages.length-1]); // find last element using length -1, should be 14 
 
 // 4.  How do you access the first element of any array?
+console.log(ages.slice(0,1));//using slice to find the element in any part of the index, (start,finisth), this gives index 0
 
 // 5.  Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
-
+const names = ["jared", "judy", "lee"];//create new array
+console.log(names);//print array to console
+const namelengths = [5,4,3];//create new array
+console.log(namelengths);//print array
 // For example:
+//help
 
 // let names = ["Kelly", "Sam", "Kate"];    // starting with this array
 // let nameLengths = [5, 3, 4];             // create a new array
